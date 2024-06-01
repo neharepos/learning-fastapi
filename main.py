b867c336289b66e161213a127216a923a0b4b88a
neharepos <nehakeshrimail@gmail.com>
2024-06-01 15:16:49 +0530

    understood get and post request

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+from typing import Optional
+
 app = FastAPI()
+
+ls = []
+
+class Post(BaseModel):
+    name : str
+    details : str
+    age : int = 22
+    rating : Optional[int]= None
+    
 
 @app.get('/')
 def home():
@@ -8,3 +20,11 @@ def home():
 @app.get('/items/{itemname}')
 def items(itemname):
     return {'item':itemname}
+
+@app.post('/posts')
+def post(post: Post):
+    print(f"name : {post.name} \ndetails : {post.details} \nage: {post.age} \nrating : {post.rating}")
+    post_dict = post.dict()
+    ls.append(post_dict)
+    print(ls)
+    return{"data":post_dict}
