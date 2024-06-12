464ca15bff6d96376678138e35df3cc93c304d9a
neharepos <nehakeshrimail@gmail.com>
2024-06-12 11:39:53 +0530

    added put and delete functionality

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
+from random import randint
 
 app = FastAPI()
 
-ls = []
+ls = [{"name" : "Nishant", "details" : "Bad boy", "age" : 77, "rating" : 4, "id" : 1},
+      {"name" : "Nisha", "details": "Good girl", "age" : 12, "rating" : 6, "id" : 2}]
+
+def search(id):
+    for i in ls:
+        if i["id"] == id:
+            return i
+
+def index_loc(id):
+    for i, p in enumerate(ls):
+        if p["id"] == id:
+            return i
 
 class Post(BaseModel):
     name : str
@@ -17,14 +29,40 @@ class Post(BaseModel):
 def home():
     return {'name':'nishant'}
 
-@app.get('/items/{itemname}')
-def items(itemname):
-    return {'item':itemname}
+@app.get('/posts')
+def get_post():
+    return {"data" : ls}
 
 @app.post('/posts')
 def post(post: Post):
     print(f"name : {post.name} \ndetails : {post.details} \nage: {post.age} \nrating : {post.rating}")
     post_dict = post.dict()
+    post_dict["id"] = randint(1,20000000000)
     ls.append(post_dict)
     print(ls)
     return{"data":post_dict}
+
+@app.get('/posts/{id}')
+def get_post_id(id : int):
+    result = search(id)
+    return {"data" : result}
+
+@app.delete("/posts/{id}")
+def delete_post(id : int):
+    location = index_loc(id)
+    print(location)
+    ls.pop(location)
+    return {"message" : "the data is deleted" }
+
+@app.put("/posts/{id}")
+def update_post(id : int,update_post : Post):
+    loc = index_loc(id)
+    upd_data = update_post.dict()
+    upd_data["id"] = id
+    ls[loc] = upd_data
+    return {"data" : upd_data}
+
+
+
+
+
